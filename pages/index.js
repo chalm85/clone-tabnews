4e2cc103791ab857383a7c4d246fb1093547eff0
Cassio Henrique de Almeida <65177748+chalm85@users.noreply.github.com>
2024-03-06 14:33:37 +0000

    adiciona a página inicial

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+function Home () {
+    return <div className="msg-wrapper">
+        <h1 style={{color: "#ffffff"}}>
+            <blockquote>
+                <p style={{fontStyle: "italic"}}>O design é a alma fundamental de uma criação humana, que acaba se expressando em camadas externas sucessivas do produto ou serviço.</p>
+                <footer style={{fontSize: "75%", fontWeight: "400", textAlign: "right"}}>Steve Jobs</footer>
+            </blockquote>
+        </h1>
+    </div>
+}
+
+export default Home;
